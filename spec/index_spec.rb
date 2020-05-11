2fe070dcd7d167cd22c5f91ce00a7e98a17b7ef4
Hemant soni <sonihemant4741@gmail.com>
2020-05-12 01:39:24 +0530

    test for my_each_with_index

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -24,4 +24,21 @@ describe Enumerable do
     end
 
   end
+
+  describe '#my_each_with_index' do
+    let(:num_arr) {[2,6,7,8]}
+
+    context "array containing number" do
+      it 'Print all the numbers with index less than 7' do
+        expect { num_arr.my_each_with_index {|item, index| puts "index: #{index} for #{item}" if item < 7 }}.to output("index: 0 for 2\nindex: 1 for 6\n").to_stdout
+      end
+    end
+
+    context "if no block is given" do
+      it 'return enumerator when no block is given' do
+        expect(num_arr.my_each_with_index).to be_an Enumerator
+      end
+    end
+
+  end
 end
